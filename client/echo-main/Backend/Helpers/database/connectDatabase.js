8f6850cb0c299f3c8e8386ae946d77399e07fa12
tyler007durden <132481727+tyler007durden@users.noreply.github.com>
2024-04-27 11:40:28 +0530

    deploy

---
+++ client/echo-main/Backend/Helpers/database/connectDatabase.js
@@ -0,0 +1,18 @@
+// connectDatabase.js
+
+const mongoose = require('mongoose');
+
+const connectDatabase = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB Connected...');
+    } catch (error) {
+        console.error(error.message);
+        process.exit(1); // Exit process with failure
+    }
+};
+
+module.exports = connectDatabase;
---
+++ client/echo-main/Backend/server.js
@@ -0,0 +1,38 @@
+// server.js
+
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const path = require('path');
+
+const indexRoute = require('./Routers/index');
+const connectDatabase = require('./Helpers/database/connectDatabase');
+const customErrorHandler = require('./Middlewares/Errors/customErrorHandler');
+
+dotenv.config({
+    path: './Config/config.env',
+});
+
+connectDatabase();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/', indexRoute);
+
+app.use(customErrorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const server = app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT} in ${process.env.NODE_ENV} mode`);
+});
+
+process.on('unhandledRejection', (err, promise) => {
+    console.error(`Logged Error: ${err}`);
+    server.close(() => process.exit(1));
+});
